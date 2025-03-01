34394dd82028b65fa46d1d4fb0830c6d19870e27
Dipprokash sardar <dipprokash2015@gmail.com>
2025-03-01 23:12:15 +0530

    FCM package added

--- src/helper/Notification.ts
+++ src/helper/Notification.ts
@@ -1,5 +1,7 @@
 import notifee from '@notifee/react-native';
 import NotificationSounds from 'react-native-notification-sounds';
+import messaging from '@react-native-firebase/messaging';
+import {PermissionsAndroid} from 'react-native';
 
 export async function onDisplayNotification() {
   // Request permissions (required for iOS)
@@ -10,7 +12,7 @@ export async function onDisplayNotification() {
   const channelId = await notifee.createChannel({
     id: 'sound',
     name: 'Default Channel',
-    sound: soundsList[0].url,
+    sound: soundsList[1].url,
   });
 
   // Display a notification
