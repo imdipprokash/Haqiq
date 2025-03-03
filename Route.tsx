849fbe52b15af8dd6de0e23365da860b71d3ff1e
Dipprokash sardar <dipprokash2015@gmail.com>
2025-03-03 22:56:29 +0530

    Notification added for Android and IOS

--- Route.tsx
+++ Route.tsx
@@ -1,4 +1,4 @@
-import {I18nManager, StyleSheet} from 'react-native';
+import {I18nManager, Platform, StyleSheet} from 'react-native';
 import React, {useEffect} from 'react';
 import LandingSrc from './src/screen/no-auth/LandingSrc';
 import LanguageSrc from './src/screen/no-auth/LanguageSrc';
@@ -6,14 +6,26 @@ import {useAppSelector} from './src/redux/store';
 import HomeSrc from './src/screen/auth/HomeSrc';
 import CategorySrc from './src/screen/auth/CategorySrc';
 import UpdateLanguageSrc from './src/screen/auth/UpdateLanguageSrc';
-
 import {createStackNavigator, TransitionPresets} from '@react-navigation/stack';
+import notifee, {AuthorizationStatus} from '@notifee/react-native';
 
 const Stack = createStackNavigator();
 const Route = () => {
   const {accessToken, languageCode} = useAppSelector(s => s.auth);
+  async function requestNotificationPermission() {
+    const settings = await notifee.requestPermission();
+
+    if (settings.authorizationStatus === AuthorizationStatus.DENIED) {
+      console.log('User denied notifications');
+    } else if (
+      settings.authorizationStatus === AuthorizationStatus.AUTHORIZED
+    ) {
+      console.log('User granted notifications');
+    }
+  }
 
   useEffect(() => {
+    requestNotificationPermission();
     if (languageCode === 'ar') {
       I18nManager.forceRTL(true), I18nManager.allowRTL(true);
     } else {
